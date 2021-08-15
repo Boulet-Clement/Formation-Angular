2206fd7babf3df580ce45fe7ef2b7cc057605999
Boulet-Clement <clement.boulet60510@gmail.com>
2021-08-15 11:21:45 +0200

    Debut de la gestion des routes

--- projet-angular/src/app/app.component.ts
+++ projet-angular/src/app/app.component.ts
@@ -1,36 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-import { AppareilService } from './services/appareil.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit{
+export class AppComponent{
   title = 'projet-angular';
 
-  isAuth = false;
+  constructor() {}
 
-  lastUpdate = new Date();
-
-  appareils: any;
-
-  constructor(private appareilService: AppareilService){
-    setTimeout(
-      ()=> {
-        this.isAuth = true;
-      },4000
-    );
-  }
-
-  ngOnInit(){
-    this.appareils = this.appareilService.appareils;
-  }
-
-  onAllumer(){
-    this.appareilService.switchOnAll();
-  }
-  onEteindre(){
-    this.appareilService.switchOffAll();
-  }
 }
